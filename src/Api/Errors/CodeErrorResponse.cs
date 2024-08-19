7f2cd9c1c0c1fa32984f978cc3714f3454f833d3
Jerson <jersonramirezs97@gmail.com>
2024-08-19 14:46:35 -0500

    Feat: se agrego manejador de errores

---
+++ src/Api/Errors/CodeErrorResponse.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+
+namespace Ecommerce.Api.Errors
+{
+    public class CodeErrorResponse
+    {
+        [JsonProperty(PropertyName = "statusCode")]
+        public int StatusCode { get; set; }
+        [JsonProperty(PropertyName = "message")]
+        public string[]? Message { get; set; }
+
+        public CodeErrorResponse(int statusCode, string[]? message = null)
+        {
+            StatusCode = statusCode;
+
+            if (message != null)
+            {
+
+                Message = new string[0];
+                var text = getDefaultMessageStatusCode(statusCode);
+                Message[0] = text;
+            }
+            else {
+
+                Message = message;
+            }
+
+        }
+
+        private string getDefaultMessageStatusCode(int statusCode) {
+
+            return statusCode switch
+            {
+                400 => "El Request enviado tiene errores",
+                401 => "No tienes authorization para este recurso",
+                404 => "No se encontro el recurso solicitado",
+                500 => "Se produjieron errores en el servidor",
+                _ => string.Empty,
+            };
+        }
+    }
+
+    
+}
