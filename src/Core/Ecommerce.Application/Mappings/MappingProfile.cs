cf37705378b3922027ddf2781a736c61253a559b
Jerson <jersonramirezs97@gmail.com>
2024-08-19 17:22:59 -0500

    Feat: se agrego la funcionalidad para la paginación

---
+++ src/Core/Ecommerce.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Ecommerce.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            
+        }
+    }
+}
---
+++ src/Core/Ecommerce.Application/Specifications/SpecificationParams.cs
@@ -0,0 +1,18 @@
+﻿namespace Ecommerce.Application.Specifications
+{
+    public class SpecificationParams
+    {
+        public string? Sort { get; set; }
+        public int PageIndex { get; set; }
+        private const int MaxpageSize = 50;
+        private int _pageSize = 3;
+
+        public int PageSize { 
+        
+            get => _pageSize;
+            set => _pageSize = (value > MaxpageSize) ? MaxpageSize : value;
+        }
+
+        public string? Search { get; set; }
+    }
+}
