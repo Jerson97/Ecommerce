c3af92d336e9a445f265a19f4f6d66e2d43ca042
Jerson <jersonramirezs97@gmail.com>
2024-09-07 12:14:35 -0500

    Feat: se agrego el api para registra al usuario

---
+++ src/Core/Ecommerce.Application/Features/Auths/Users/Commands/RegisterUser/RegisterUserCommand.cs
@@ -0,0 +1,20 @@
+﻿using Ecommerce.Application.Features.Auths.Users.Vms;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace Ecommerce.Application.Features.Auths.Users.Commands.RegisterUser
+{
+    public class RegisterUserCommand : IRequest<AuthResponse>
+    {
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public IFormFile? Photo { get; set; }
+        public string? PhotoUrl { get; set; }
+        public string? PhotoId { get; set; }
+        public string? Password { get; set; }
+        public string? UserName { get; set; }
+
+    }
+}
---
+++ src/Core/Ecommerce.Application/Features/Auths/Users/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,77 @@
+﻿using AutoMapper;
+using Ecommerce.Application.Contracts.Identity;
+using Ecommerce.Application.Exceptions;
+using Ecommerce.Application.Features.Auths.Users.Vms;
+using Ecommerce.Application.Persistence;
+using Ecommerce.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Ecommerce.Application.Features.Auths.Users.Commands.RegisterUser
+{
+    public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, AuthResponse>
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IAuthService _authService;
+
+        public RegisterUserCommandHandler(UserManager<User> userManager, RoleManager<IdentityRole> roleManager, IAuthService authService)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _authService = authService;
+        }
+
+        public async Task<AuthResponse> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            var email = await _userManager.FindByEmailAsync(request.Email!) is null ? false : true;
+
+            if (email)
+            {
+                throw new BadRequestException("El Email del usuario ya existe en la base de datos");
+            }
+
+            var userName = await _userManager.FindByNameAsync(request.UserName!) is null ? false : true;
+
+            if (userName)
+            {
+                throw new BadRequestException("El UserName del usuario ya existe en la base de datos");
+            }
+
+            var user = new User
+            {
+                Name = request.Name,
+                LastName = request.LastName,
+                Phone = request.Phone,
+                Email = request.Email,
+                UserName = request.UserName,
+                AvatarUrl = request.PhotoUrl
+            };
+
+            var result = await _userManager.CreateAsync(user!, request.Password!);
+
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(user, AppRole.GenericUser);
+
+                var roles = await _userManager.GetRolesAsync(user);
+
+                var rolesList = new List<string>(roles);
+
+                return new AuthResponse
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                    LastName = user.LastName,
+                    Phone = user.Phone,
+                    Email = user.Email,
+                    Avatar = user.AvatarUrl,
+                    Token = _authService.CreateToken(user, rolesList),
+                    Roles = roles
+                };
+            }
+
+            throw new Exception("No se pudo registrar el usuario");
+        }
+    }
+}
